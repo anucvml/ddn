1240f0e9f06f07e53a713102755853b17c1ef860
Stephen Gould <u4971180@anu.edu.au>
2020-10-30 21:30:35 +1100

    added example of robust vector pooling

--- ddn/basic/robust_nodes.py
+++ ddn/basic/robust_nodes.py
@@ -87,3 +87,70 @@ class RobustAverage(NonUniqueDeclarativeNode):
             dy = np.array([(self.alpha_sq - zi) / (self.alpha_sq * self.alpha_sq) * np.exp(-0.5 * zi / self.alpha_sq) for zi in z])
 
         return dy.reshape((1, self.dim_x)) / np.sum(dy)
+
+
+class RobustVectorAverage(NonUniqueDeclarativeNode):
+    """
+    Solves for the multi-dimensional robust average,
+        minimize f(x, y) = \sum_{i=1}^{n} phi(\|y - x_i\|; alpha)
+    where phi(z; alpha) is one of the following robust penalties,
+        'quadratic':    1/2 z^2
+        'pseudo-huber': alpha^2 (\sqrt(1 + (z/alpha)^2 - 1)
+        'huber':        1/2 z^2 for |z| <= alpha and alpha |z| - 1/2 alpha^2 otherwise
+        'welsch':       1 - exp(-z^2 / 2 alpha^2)
+        'trunc-quad':   1/2 z^2 for |z| <= alpha and 1/2 alpha^2 otherwise
+
+    The input is assumed to be flattened from an (n \times m)-matrix to an nm-vector.
+    """
+
+    restarts = 10 # number of random restarts when solving non-convex penalties
+
+    def __init__(self, n, m, penalty='huber', alpha=1.0):
+        assert (alpha > 0.0)
+        self.alpha = alpha
+        self.alpha_sq = alpha ** 2
+        self.penalty = penalty.lower()
+        if (self.penalty == 'quadratic'):
+            self.phi = lambda z: 0.5 * np.power(z, 2.0)
+        elif (self.penalty == 'pseudo-huber'):
+            self.phi = lambda z: self.alpha_sq * (np.sqrt(1.0 + np.power(z, 2.0) / self.alpha_sq) - 1.0)
+        elif (self.penalty == 'huber'):
+            self.phi = lambda z: np.where(np.abs(z) <= alpha, 0.5 * np.power(z, 2.0), alpha * np.abs(z) - 0.5 * self.alpha_sq)
+        elif (self.penalty == 'welsch'):
+            self.phi = lambda z: 1.0 - np.exp(-0.5 * np.power(z, 2.0) / self.alpha_sq)
+        elif (self.penalty == 'trunc-quad'):
+            self.phi = lambda z: np.minimum(0.5 * np.power(z, 2.0), 0.5 * self.alpha_sq)
+        else:
+            assert False, "unrecognized penalty function {}".format(penalty)
+
+        super().__init__(n*m, m) # make sure node is properly constructed
+        self.eps = 1.0e-4 # relax tolerance on optimality test
+
+    def objective(self, x, y):
+        assert (len(x) == self.dim_x) and (len(y) == self.dim_y)
+        # the inclusion of 1.0e-9 prevents division by zero during automatic differentiation when a y lands exactly on a data point xi
+        return np.sum([self.phi(np.sqrt(np.dot(y - xi, y - xi) + 1.0e-9)) for xi in x.reshape((int(self.dim_x / self.dim_y), self.dim_y))])
+
+    def solve(self, x):
+        assert(len(x) == self.dim_x)
+
+        J = lambda y : self.objective(x, y)
+        dJ = lambda y : self.fY(x, y)
+
+        y_star = np.mean(x.reshape((int(self.dim_x / self.dim_y), self.dim_y)), 0)
+        if (self.penalty != 'quadratic'):
+            result = opt.minimize(J, y_star, args=(), method='L-BFGS-B', jac=dJ, options={'maxiter': 100, 'disp': False})
+            if not result.success: print(result.message)
+            y_star, J_star = result.x, result.fun
+
+        # run with different intial guesses for non-convex penalties
+        if (self.penalty == 'welsch') or (self.penalty == 'trunc-quad'):
+            guesses = np.random.permutation(x.reshape((int(self.dim_x / self.dim_y), self.dim_y)))
+            if self.dim_x > self.restarts: guesses = guesses[0:self.restarts, :]
+            for y_init in guesses:
+                result = opt.minimize(J, y_init, args=(), method='L-BFGS-B', jac=dJ, options={'maxiter': 100, 'disp': False})
+                if not result.success: print(result.message)
+                if (result.fun < J_star):
+                    y_star, J_star = result.x, result.fun
+
+        return y_star, None
