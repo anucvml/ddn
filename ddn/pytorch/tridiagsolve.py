fa001e3182b83247804cd539b33a8b4aca8f2adb
Stephen Gould <stephen.gould@anu.edu.au>
2024-02-06 23:31:57 +1100

    implemented cyclic reduction for block tridiagonal matrices; changed ordering of blocks in input for easier parallelization and consistency with the non-block case

--- ddn/pytorch/tridiagsolve.py
+++ ddn/pytorch/tridiagsolve.py
@@ -28,13 +28,13 @@ def tridiagsolve(b, a, c, d, method='cyclic'):
     :param b: main diagonal, (b_1, ..., b_n), of size (B x N)
     :param a: lower diagonal, (a_2, ..., a_n), of size (B x N-1)
     :param c: upper diagonal, (c_1, ..., c_{n-1}), of size (B x N-1)
-    :param d: right-hand-size, (d_1, ..., d_n), of size (B x N x M)
+    :param d: right-hand-size, (d_1, ..., d_n), of size (B x N x K) or (B x N x 1)
     :param method: 'cyclic' or 'thomas' or 'linalg'
-    :return: x = (x_1, ..., x_n) of size (B x N x M)
+    :return: x = (x_1, ..., x_n) of size (B x N x K) or (B x N x 1)
     """
 
-    assert len(d.shape) == 3, "argument 'd' must have shape (B, N, M) or (B, N, 1)"
-    B, N, M = d.shape
+    assert len(d.shape) == 3, "argument 'd' must have shape (B, N, K) or (B, N, 1)"
+    B, N, K = d.shape
     assert b.shape == (B, N), "argument 'b' must have shape (B, N)"
     assert a.shape == (B, N-1), "argument 'a' must have shape (B, N-1)"
     assert c.shape == (B, N-1), "argument 'c' must have shape (B, N-1)"
@@ -67,7 +67,6 @@ def tridiagsolve(b, a, c, d, method='cyclic'):
         return d_dash / b_dash.view(B, N, 1)
 
     elif method == 'thomas':
-
         # initialize
         x = torch.empty_like(d)
         c_dash = torch.empty_like(c)
@@ -100,7 +99,7 @@ def tridiagsolve(b, a, c, d, method='cyclic'):
         raise NameError("unknown method '{}'".format(method))
 
 
-def blocktridiagsolve(b, a, c, d, method='cyclic'):
+def blocktridiagsolve(b, a, c, d, method='thomas'):
     """
     Batch block tridiagonal matrix solver based on either cyclic reduction (method='cyclic'), the Thomas algorithm
     (method='thomas') or the standard PyTorch linear algebra solver (method='linalg') to solve systems of the form,
@@ -112,31 +111,57 @@ def blocktridiagsolve(b, a, c, d, method='cyclic'):
         |              .      | |      |   |  .   |
         |             ... B_n | | x_mn |   | d_mn |
 
-    where B_i, A_i and C_i and m-by-m matrices. See `tridiagsolve` for the case of 1-by-1 blocks.
+    where B_i, A_i and C_i and m-by-m matrices. See `tridiagsolve` for the case of 1-by-1 blocks. The cyclic reduction
+    method may become numerically unstable when the B_i are not very well conditioned and the Thomas algorithm is
+    recommended.
 
-    :param b: main diagonal, (B_1, ..., B_n), of size (B x M x M x N)
-    :param a: lower diagonal, (A_2, ..., A_n), of size (B x M x M x N-1)
-    :param c: upper diagonal, (C_1, ..., C_{n-1}), of size (B x M x M x N-1)
-    :param d: right-hand-size, (d_1, ..., d_mn), of size (B x MN x K)
+    :param b: main diagonal, (B_1, ..., B_n), of size (B x N x M x M)
+    :param a: lower diagonal, (A_2, ..., A_n), of size (B x N-1 x M x M)
+    :param c: upper diagonal, (C_1, ..., C_{n-1}), of size (B x N-1 x M x M)
+    :param d: right-hand-size, (d_1, ..., d_mn), of size (B x MN x K) or (B x MN x 1)
     :param method: 'cyclic' or 'thomas' or 'linalg'
-    :return: x = (x_1, ..., x_n) of size (B x MN x K)
+    :return: x = (x_1, ..., x_n) of size (B x MN x K) or (B x MN x 1)
     """
 
     assert len(d.shape) == 3, "argument 'd' must have shape (B, MN, K) or (B, MN, 1)"
     B, MN, K = d.shape
     assert len(b.shape) == 4, "argument 'b' must have shape (B, M, M, N)"
-    M, N = b.shape[1], b.shape[3]
+    M, N = b.shape[3], b.shape[1]
     assert M * N == MN, "argument 'd' must have shape (B, MN, K) or (B, MN, 1)"
-    assert b.shape == (B, M, M, N), "argument 'b' must have shape (B, M, M, N)"
-    assert a.shape == (B, M, M, N-1), "argument 'a' must have shape (B, M, M, N-1)"
-    assert c.shape == (B, M, M, N-1), "argument 'c' must have shape (B, M, M, N-1)"
+    assert b.shape == (B, N, M, M), "argument 'b' must have shape (B, N, M, M)"
+    assert a.shape == (B, N-1, M, M), "argument 'a' must have shape (B, N-1, M, M)"
+    assert c.shape == (B, N-1, M, M), "argument 'c' must have shape (B, N-1, M, M)"
 
     if method == 'cyclic':
-        # TODO
-        raise NotImplementedError("'{}' not implemented yet".format(method))
+        # initialize
+        a_dash, c_dash = a, c
+        b_dash = b.clone()
+        d_dash = d.clone().reshape(B, N, M, K)
+
+        # repeat until problems of size 1
+        h = 1
+        while (h < N):
+            # eliminate odd/even terms
+            alpha = -1.0 * torch.linalg.solve(b_dash[:, :-h, :, :], a_dash, left=False)
+            beta = -1.0 * torch.linalg.solve(b_dash[:, h:, :, :], c_dash, left=False)
+
+            b_dash[:, h:, :, :] += alpha @ c_dash
+            b_dash[:, :-h, :, :] += beta @ a_dash
+
+            d_prev = d_dash.clone()
+            d_dash[:, h:, :, :] += alpha @ d_prev[:, :-h, :, :]
+            d_dash[:, :-h, :, :] += beta @ d_prev[:, h:, :, :]
+
+            if (h < alpha.shape[1]):
+                a_dash = alpha[:, h:, :, :] @ a_dash[:, :-h, :, :]
+                c_dash = beta[:, :-h, :, :] @ c_dash[:, h:, :, :]
+
+            h *= 2
+
+        # solve
+        return torch.linalg.solve(b_dash, d_dash).reshape(B, M * N, K)
 
     elif method == 'thomas':
-
         # initialize
         x = torch.empty_like(d)
         c_dash = torch.empty_like(c)
@@ -144,34 +169,33 @@ def blocktridiagsolve(b, a, c, d, method='cyclic'):
 
         # forward elimination
         # TODO: solve for c and d jointly
-        c_dash[:, :, :, 0] = torch.linalg.solve(b[:, :, :, 0], c[:, :, :, 0])
-        d_dash[:, 0:M, :] = torch.linalg.solve(b[:, :, :, 0], d[:, 0:M, :])
+        c_dash[:, 0, :, :] = torch.linalg.solve(b[:, 0, :, :], c[:, 0, :, :])
+        d_dash[:, 0:M, :] = torch.linalg.solve(b[:, 0, :, :], d[:, 0:M, :])
 
         for i in range(1, N-1):
             # TODO: solve for c and d jointly
-            w = b[:, :, :, i] - a[:, :, :, i-1] @ c_dash[:, :, :, i-1]
-            c_dash[:, :, :, i] = torch.linalg.solve(w, c[:, :, :, i])
-            d_dash[:, i*M:(i+1)*M, :] = torch.linalg.solve(w, d[:, i*M:(i+1)*M, :] - a[:, :, :, i-1] @ d_dash[:, (i-1)*M:i*M, :])
+            w = b[:, i, :, :] - a[:, i-1, :, :] @ c_dash[:, i-1, :, :]
+            c_dash[:, i, :, :] = torch.linalg.solve(w, c[:, i, :, :])
+            d_dash[:, i*M:(i+1)*M, :] = torch.linalg.solve(w, d[:, i*M:(i+1)*M, :] - a[:, i-1, :, :] @ d_dash[:, (i-1)*M:i*M, :])
 
-        w = b[:, :, :, N-1] - a[:, :, :, N-2] @ c_dash[:, :, :, N-2]
-        d_dash[:, (N-1)*M:MN, :] = torch.linalg.solve(w, d[:, (N-1)*M:MN, :] - a[:, :, :, N-2] @ d_dash[:, (N-2)*M:(N-1)*M, :])
+        w = b[:, N-1, :, :] - a[:, N-2, :, :] @ c_dash[:, N-2, :, :]
+        d_dash[:, (N-1)*M:MN, :] = torch.linalg.solve(w, d[:, (N-1)*M:MN, :] - a[:, N-2, :, :] @ d_dash[:, (N-2)*M:(N-1)*M, :])
 
         # backward substitution
         x[:, (N-1)*M:MN, :] = d_dash[:, (N-1)*M:MN, :]
         for i in range(N-1, 0, -1):
-            x[:, (i-1)*M:i*M, :] = d_dash[:, (i-1)*M:i*M, :] - c_dash[:, :, :, i-1] @ x[:, i*M:(i+1)*M, :]
+            x[:, (i-1)*M:i*M, :] = d_dash[:, (i-1)*M:i*M, :] - c_dash[:, i-1, :, :] @ x[:, i*M:(i+1)*M, :]
 
         return x
 
     elif method == 'linalg':
         A = torch.zeros((B, MN, MN), dtype=d.dtype, device=d.device)
         for i in range(N):
-            A[:, i*M:(i+1)*M, i*M:(i+1)*M] = b[:, :, :, i]
+            A[:, i*M:(i+1)*M, i*M:(i+1)*M] = b[:, i, :, :]
         for i in range(N-1):
-            A[:, (i+1)*M:(i+2)*M, i*M:(i+1)*M] = a[:, :, :, i]
-            A[:, i*M:(i+1)*M, (i+1)*M:(i+2)*M] = c[:, :, :, i]
+            A[:, (i+1)*M:(i+2)*M, i*M:(i+1)*M] = a[:, i, :, :]
+            A[:, i*M:(i+1)*M, (i+1)*M:(i+2)*M] = c[:, i, :, :]
         return torch.linalg.solve(A, d)
-        pass
 
     else:
         raise NameError("unknown method '{}'".format(method))
@@ -209,7 +233,7 @@ class BlockTriDiagSolveFcn(torch.autograd.Function):
     """
 
     @staticmethod
-    def forward(ctx, b, a, c, d, method='cyclic'):
+    def forward(ctx, b, a, c, d, method='thomas'):
         with torch.no_grad():
             x = blocktridiagsolve(b, a, c, d, method)
         ctx.save_for_backward(b, a, c, d, x)
@@ -220,22 +244,22 @@ class BlockTriDiagSolveFcn(torch.autograd.Function):
     def backward(ctx, grad_x):
         b, a, c, d, x = ctx.saved_tensors
 
-        B, M, _, N = b.shape
-        w = blocktridiagsolve(b.transpose(1, 2), c.transpose(1, 2), a.transpose(1, 2), grad_x, ctx.method)
+        B, N, M, _ = b.shape
+        w = blocktridiagsolve(b.transpose(2, 3), c.transpose(2, 3), a.transpose(2, 3), grad_x, ctx.method)
         grad_b, grad_a, grad_c = None, None, None
 
         if ctx.needs_input_grad[0]:
             grad_b = torch.empty_like(b)
             for i in range(N):
-                grad_b[:, :, :, i] = -1.0 * torch.einsum("bik,bjk->bij",  w[:, i*M:(i+1)*M, :], x[:, i*M:(i+1)*M, :])
+                grad_b[:, i, :, :] = -1.0 * torch.einsum("bik,bjk->bij",  w[:, i*M:(i+1)*M, :], x[:, i*M:(i+1)*M, :])
         if ctx.needs_input_grad[1]:
             grad_a = torch.empty_like(a)
             for i in range(N - 1):
-                grad_a[:, :, :, i] = -1.0 * torch.einsum("bik,bjk->bij",  w[:, (i+1)*M:(i+2)*M, :], x[:, i*M:(i+1)*M, :])
+                grad_a[:, i, :, :] = -1.0 * torch.einsum("bik,bjk->bij",  w[:, (i+1)*M:(i+2)*M, :], x[:, i*M:(i+1)*M, :])
         if ctx.needs_input_grad[2]:
             grad_c = torch.empty_like(c)
             for i in range(N - 1):
-                grad_c[:, :, :, i] = -1.0 * torch.einsum("bik,bjk->bij",  w[:, i*M:(i+1)*M, :], x[:, (i+1)*M:(i+2)*M, :])
+                grad_c[:, i, :, :] = -1.0 * torch.einsum("bik,bjk->bij",  w[:, i*M:(i+1)*M, :], x[:, (i+1)*M:(i+2)*M, :])
 
         grad_d = w if ctx.needs_input_grad[3] else None
 
@@ -249,6 +273,7 @@ class BlockTriDiagSolveFcn(torch.autograd.Function):
 
 if __name__ == '__main__':
 
+    # --- testing accuracy ---
     B, N, K = 2, 16, 5
     type = torch.float64
     device = torch.device("cpu")
@@ -289,24 +314,25 @@ if __name__ == '__main__':
     # block tridiagonal matrices
     M = 3
 
-    b = 0.1 * torch.rand((B, M, M, N), dtype=type, device=device) + 2.0 * torch.eye(M, dtype=type, device=device).view(1, M, M, 1)
-    a = -1.0 * torch.rand((B, M, M, N - 1), dtype=type, device=device)
-    c = -1.0 * torch.rand((B, M, M, N - 1), dtype=type, device=device)
-    d = torch.rand((B, M*N, K), dtype=type, device=device)
+    b = 0.1 * torch.rand((B, N, M, M), dtype=type, device=device) + \
+        2.0 * torch.eye(M, dtype=type, device=device).view(1, 1, M, M)
+    a = -1.0 * torch.rand((B, N - 1, M, M), dtype=type, device=device)
+    c = -1.0 * torch.rand((B, N - 1, M, M), dtype=type, device=device)
+    #d = torch.rand((B, M*N, K), dtype=type, device=device)
+    d = torch.linspace(1, B*N*M*K, B*N*M*K, dtype=type, device=device).reshape(B, N * M, K)
 
     A = torch.zeros((B, M*N, M*N), dtype=type, device=device)
     for n in range(N):
-        A[:, n * M:(n + 1) * M, n * M:(n + 1) * M] = b[:, :, :, n]
+        A[:, n * M:(n + 1) * M, n * M:(n + 1) * M] = b[:, n, :, :]
     for n in range(N - 1):
-        A[:, (n + 1) * M:(n + 2) * M, n * M:(n + 1) * M] = a[:, :, :, n]
-        A[:, n * M:(n + 1) * M, (n + 1) * M:(n + 2) * M] = c[:, :, :, n]
+        A[:, (n + 1) * M:(n + 2) * M, n * M:(n + 1) * M] = a[:, n, :, :]
+        A[:, n * M:(n + 1) * M, (n + 1) * M:(n + 2) * M] = c[:, n, :, :]
 
     print("Checking blocktridiagsolve accuracy on arbitrary input...")
     print(b.shape, d.shape)
 
-    # TODO: uncomment when implemented
-    #x = blocktridiagsolve(b, a, c, d, 'cyclic')
-    #print(torch.max(torch.abs(A @ x - d)).item())
+    x = blocktridiagsolve(b, a, c, d, 'cyclic')
+    print(torch.max(torch.abs(A @ x - d)).item())
 
     x = blocktridiagsolve(b, a, c, d, 'thomas')
     print(torch.max(torch.abs(A @ x - d)).item())
@@ -316,6 +342,7 @@ if __name__ == '__main__':
 
     #exit(0)
 
+    # --- testing gradient calculation ---
     from torch.autograd import gradcheck
 
     torch.manual_seed(22)
@@ -338,27 +365,26 @@ if __name__ == '__main__':
     test = gradcheck(TriDiagSolveFcn().apply, (b, a, c, d, 'thomas'), eps=1e-6, atol=1e-3, rtol=1e-6)
     print(test)
 
-    b = 0.1 * torch.rand((B, M, M, N), dtype=type, device=device, requires_grad=True) + 2.0 * torch.eye(M, dtype=type, device=device).view(1, M, M, 1)
-    a = -1.0 * torch.rand((B, M, M, N - 1), dtype=type, device=device, requires_grad=True)
-    c = -1.0 * torch.rand((B, M, M, N - 1), dtype=type, device=device, requires_grad=True)
+    b = 0.1 * torch.rand((B, N, M, M), dtype=type, device=device, requires_grad=True) + 2.0 * torch.eye(M, dtype=type, device=device).view(1, 1, M, M)
+    a = -1.0 * torch.rand((B, N - 1, M, M), dtype=type, device=device, requires_grad=True)
+    c = -1.0 * torch.rand((B, N - 1, M, M), dtype=type, device=device, requires_grad=True)
     d = torch.rand((B, M*N, K), dtype=type, device=device, requires_grad=True)
 
-    # TODO: uncomment when implemented
-    #print("Checking gradients (block cyclic)...")
-    #test = gradcheck(BlockTriDiagSolveFcn().apply, (b, a, c, d, 'cyclic'), eps=1e-6, atol=1e-3, rtol=1e-6)
-    #print(test)
+    print("Checking gradients (block cyclic)...")
+    test = gradcheck(BlockTriDiagSolveFcn().apply, (b, a, c, d, 'cyclic'), eps=1e-6, atol=1e-3, rtol=1e-6)
+    print(test)
 
     print("Checking gradients (block thomas)...")
     test = gradcheck(BlockTriDiagSolveFcn().apply, (b, a, c, d, 'thomas'), eps=1e-6, atol=1e-3, rtol=1e-6)
     print(test)
 
-
     #exit(0)
 
+    # --- testing running time ---
     import time
-    print("Testing running time...")
+    print("Testing TriDiagSolve running time...")
 
-    N = 256
+    N, M = 256, 7
     type = torch.float32
 
     for B in (1, 10, 100, 1000):
@@ -400,6 +426,57 @@ if __name__ == '__main__':
         start = time.time()
         z.sum().backward()
         dz_elapsed = time.time() - start
+
+        print('accuracy cyclic/thomas/linalg.solve: {:.3e}/{:.3e}/{:.3e}'.format(torch.max(torch.abs(A @ x - d)).item(),
+            torch.max(torch.abs(A @ y - d)).item(), torch.max(torch.abs(A @ z - d)).item()))
+        print(' forward cyclic/thomas/linalg.solve: {:.3e}/{:.3e}/{:.3e}'.format(x_elapsed, y_elapsed, z_elapsed))
+        print('backward cyclic/thomas/linalg.solve: {:.3e}/{:.3e}/{:.3e}'.format(dx_elapsed, dy_elapsed, dz_elapsed))
+
+
+    print("Testing BlockTriDiagSolve running time...")
+    for B in (1, 10, 100):
+        b = 0.1 * torch.rand((B, N, M, M), dtype=type, device=device, requires_grad=True) + \
+             2.0 * M * torch.eye(M, dtype=type, device=device).view(1, 1, M, M)
+        a = -1.0 * torch.rand((B, N - 1, M, M), dtype=type, device=device, requires_grad=True)
+        c = -1.0 * torch.rand((B, N - 1, M, M), dtype=type, device=device, requires_grad=True)
+        d = torch.rand((B, M * N, 1), dtype=type, device=device, requires_grad=True)
+
+        print("...data size {} {}".format(b.shape, d.shape))
+
+        start = time.time()
+        x = BlockTriDiagSolveFcn.apply(b.clone().detach().requires_grad_(True), a.clone().detach().requires_grad_(True),
+                                  c.clone().detach().requires_grad_(True), d.clone().detach().requires_grad_(True), 'cyclic')
+        x_elapsed = time.time() - start
+
+        start = time.time()
+        x.sum().backward()
+        dx_elapsed = time.time() - start
+
+        start = time.time()
+        y = BlockTriDiagSolveFcn.apply(b.clone().detach().requires_grad_(True), a.clone().detach().requires_grad_(True),
+                                  c.clone().detach().requires_grad_(True), d.clone().detach().requires_grad_(True), 'thomas')
+        y_elapsed = time.time() - start
+
+        start = time.time()
+        y.sum().backward()
+        dy_elapsed = time.time() - start
+
+        start = time.time()
+        z = BlockTriDiagSolveFcn.apply(b.clone().detach().requires_grad_(True), a.clone().detach().requires_grad_(True),
+                                  c.clone().detach().requires_grad_(True), d.clone().detach().requires_grad_(True), 'linalg')
+        z_elapsed = time.time() - start
+        print(z.shape)
+
+        start = time.time()
+        z.sum().backward()
+        dz_elapsed = time.time() - start
+
+        A = torch.zeros((B, M * N, M * N), dtype=type, device=device)
+        for n in range(N):
+            A[:, n * M:(n + 1) * M, n * M:(n + 1) * M] = b[:, n, :, :]
+        for n in range(N - 1):
+            A[:, (n + 1) * M:(n + 2) * M, n * M:(n + 1) * M] = a[:, n, :, :]
+            A[:, n * M:(n + 1) * M, (n + 1) * M:(n + 2) * M] = c[:, n, :, :]
 
         print('accuracy cyclic/thomas/linalg.solve: {:.3e}/{:.3e}/{:.3e}'.format(torch.max(torch.abs(A @ x - d)).item(),
             torch.max(torch.abs(A @ y - d)).item(), torch.max(torch.abs(A @ z - d)).item()))
